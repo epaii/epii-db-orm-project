5e73370487593f5d5311b57116ea82aef4ed33a8
suyahu <751198764@qq.com>
2023-09-15 15:29:08 +0800

    yh

--- epii-orm/src/InterfaceTypes.ts
+++ epii-orm/src/InterfaceTypes.ts
@@ -4,6 +4,7 @@ export type StringOrNull = string | null;
 export type QueryOptionsKeys = 'alias' | 'field' | 'group' | 'having';
 export type QueryJoinType = 'left' | 'right' | 'inner';
 export type BaseType = string | number | boolean ;
+export type WhereSymbol = '>' | '>=' | '<' | '<=' | '!=' | '<>' | '!>' | '!<'
 export interface PlainObject {
     [key:string]:BaseType
 }
--- epii-orm/src/map/WhereData.js
+++ epii-orm/src/map/WhereData.js
@@ -30,14 +30,46 @@ var WhereData = /** @class */ (function () {
         this.expData.push(field + " like '" + value + "'");
         return this;
     };
-    WhereData.prototype.putIn = function (field, value, type) {
-        this.expData.push(field + " " + type + value);
+    // 包含 id in (1,2,3)
+    WhereData.prototype.putIn = function (field, value) {
+        var condition_sql = "";
+        condition_sql = "(" + value.toString() + ")";
+        this.expData.push(field + " in " + condition_sql);
+        return this;
+    };
+    //不包含  id not in (1,2,3)
+    WhereData.prototype.putNotIn = function (field, value) {
+        var condition_sql = "";
+        condition_sql = "(" + value.toString() + ")";
+        this.expData.push(field + " not in " + condition_sql);
+        return this;
+    };
+    // 有符号的 
+    WhereData.prototype.putSymbol = function (field, type, value) {
+        this.expData.push(field + type + value);
+        return this;
+    };
+    // 在范围之内
+    WhereData.prototype.putBetween = function (field, sValue, eValue) {
+        var condition_sql = field + ' between ' + sValue + ' and ' + eValue;
+        this.expData.push(condition_sql);
+        return this;
+    };
+    // 不在范围之内
+    WhereData.prototype.putNotBetween = function (field, sValue, eValue) {
+        var condition_sql = field + ' not between ' + sValue + ' and ' + eValue;
+        this.expData.push(condition_sql);
         return this;
     };
     WhereData.prototype.make = function () {
         var out = new WhereData();
+        return out;
     };
     return WhereData;
 }());
 exports.WhereData = WhereData;
-console.log(new WhereData().putLike("name", '%亚%'));
+var Where = new WhereData();
+console.log(Where);
+console.log(Where.make());
+// console.log( new WhereData().putIn("id",'1,2,3'))
+console.log(new WhereData().putNotBetween("id", '2', '5'));
